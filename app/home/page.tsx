9b2076f6c63051f4ed41f305939f75b322f4392b
billybrown237 <kano.billybrown@gmail.com>
2024-05-19 14:59:21 +0100

    chore:completed portfolio

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,18 +1,18 @@
 import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavBar";
 import { navItems } from "@/data";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main
       className={
-        "relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5"
+        "relative bg-black-100 flex justify-center items-center overflow-clip flex-col mx-auto sm:px-10 px-5"
       }
     >
       <div className={"mx-w-7xl w-full"}>
@@ -23,6 +23,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
