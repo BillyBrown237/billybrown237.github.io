a47fec478ba3523a71a5da2d3b52398d59618ac2
kanodekoubillybrown <billy.kano@aitecaf.com>
2025-09-03 00:10:00 +0100

    fixed:Error occurred prerendering page "/home". ReferenceError: document is not defined

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,11 +2,14 @@ import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
-import Grid from "@/components/Grid";
+// import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavBar";
 import { navItems } from "@/data";
+import dynamic from 'next/dynamic';
+
+const Grid = dynamic(() => import('@/components/Grid'), { ssr: false });
 
 export default function Home() {
   return (
